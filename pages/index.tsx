1b92329cb15ed40bbc5d2e07d57ecc64113f0c94
Ayan Yenbekbay <ayan.yenb@gmail.com>
2019-07-07 23:54:58 +0600

    Set up responsive images for background

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,38 @@
 import {Box, Container, Image} from 'fannypack'
 import ProgressiveImage from 'react-progressive-image'
+// @ts-ignore
+import blackSandScreenshot from '../assets/black-sand-screenshot.png?resize&sizes[]=640&sizes[]=768&sizes[]=1024&sizes[]=1366&sizes[]=1600&sizes[]=1920'
 
 const Home: React.FC = _props => {
   return (
     <Box overflow="hidden">
       <ProgressiveImage
         placeholder=""
-        src={require('../assets/black-sand-screenshot.png')}>
-        {(src: string, loading: boolean) => (
-          <Box position="fixed" width="100vw" height="100vh">
+        src={blackSandScreenshot.src}
+        srcSetData={{
+          srcSet: blackSandScreenshot.srcSet,
+          sizes: '100vw',
+        }}>
+        {(
+          src: string,
+          loading: boolean,
+          srcSetData: {srcSet: string; sizes: string},
+        ) => (
+          <Box position="fixed" width="100vw" height="100vh" overflow="hidden">
             <Image
               src={require('../assets/black-sand-screenshot.png?lqip')}
+              fit="cover"
               position="absolute"
               width="100%"
               height="100%"
               filter="blur(25px)"
-              transform="scale(1.1)"
+              transform="scale(1.2)"
             />
             <Image
               src={src}
+              srcSet={srcSetData.srcSet}
+              sizes={srcSetData.sizes}
+              fit="cover"
               position="absolute"
               width="100%"
               height="100%"
