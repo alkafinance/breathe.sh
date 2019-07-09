282238d6c774ec2d161fffb6ebaa4a3f6b1736dd
Ayan Yenbekbay <ayan.yenb@gmail.com>
2019-07-10 01:16:16 +0600

    Fix content on custom error page

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -1,26 +1,42 @@
+import {Flex, Heading} from 'fannypack'
 import {NextPageContext} from 'next'
 import {ErrorProps} from 'next/error'
+import {PageLayout} from '../components/PageLayout'
 import React from 'react'
+
+const statusCodes: {[code: number]: string} = {
+  400: 'Bad Request',
+  404: 'This page could not be found',
+  405: 'Method Not Allowed',
+  500: 'Internal Server Error',
+}
 
 class Error extends React.Component<ErrorProps> {
   static getInitialProps({res, err}: NextPageContext) {
-    const statusCode = res
-      ? res.statusCode
-      : err
-      ? // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        (err as any).statusCode
-      : null
+    const statusCode =
+      res && res.statusCode ? res.statusCode : err ? err.statusCode : 404
 
     return {statusCode}
   }
 
   render() {
+    const {statusCode} = this.props
+    const title =
+      this.props.title ||
+      statusCodes[statusCode] ||
+      'An unexpected error has occurred'
+
     return (
-      <p>
-        {this.props.statusCode
-          ? `An error ${this.props.statusCode} occurred on server`
-          : 'An error occurred on client'}
-      </p>
+      <PageLayout>
+        <Flex flexDirection="column" padding="1.5rem" marginTop="1.5rem">
+          <Heading use="h1" textAlign="center">
+            {statusCode}
+          </Heading>
+          <Heading use="h4" textAlign="center">
+            {title}
+          </Heading>
+        </Flex>
+      </PageLayout>
     )
   }
 }
